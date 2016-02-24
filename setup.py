2ea63f932dadcfb6dd2de0f84430ef8ffc36786f
Evan Goad <evangoad102@gmail.com>
2016-02-24 03:47:38 -0800

    Began working on python script.

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+import os
+
+ 
+setup(
+       name='devie-dev',
+       version='0.1',  
+       scripts=['bin/devie-dev'],
+       install_requires=['unipath','setuptools','ansible']
+     )
