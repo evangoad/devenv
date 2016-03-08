d7a0484f3d4da162b16133717a1796caf308e461
Evan Goad <evangoad102@gmail.com>
2016-03-08 03:36:51 -0800

    git pull before running playbook, changed the order around

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
        name='devenv',
        version='0.1',  
        scripts=['bin/devenv'],
-       install_requires=['unipath','setuptools','ansible']
+       install_requires=['unipath','setuptools','ansible','gitpython']
      )
