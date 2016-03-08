3a2e79082135a975077d2dbaa6c7ca12d157c073
Evan Goad <evangoad102@gmail.com>
2016-03-07 17:22:37 -0800

    move python installation to my own install.sh script

--- install.sh
+++ install.sh
@@ -2,11 +2,20 @@
 #
 # script to install dotfile
 
-# Clone from Github
+# Clone devie from Github
 if [ -d "$HOME/.devie" ]; then
   echo "dotfile github repo already cloned."
 else
   git clone https://github.com/evangoad/devie ~/.devie
+fi
+
+# Install Python
+if [[ `uname` == 'Linux' ]]; then
+  sudo apt-get update
+  sudo apt-get install python
+else
+  brew update
+  brew install python
 fi
 
 # Install pip helper
